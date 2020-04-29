9e45c4a99d9a4d576a20c62f92073da5612ce239
Juliana Gonçalves <julianaxavier246@gmail.com>
2020-04-29 15:00:58 -0300

    filter

---
+++ Seção 5/index_3.js
@@ -0,0 +1,49 @@
+// var numeros=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// function filtrar_pares(n){
+//     return n % 2 === 0;
+// }
+// //console.log(filtrar_pares(3));
+// console.log(filtrar_pares(8));
+// function filtrar_impares(n){
+//     return n % 2 !== 0;
+// }
+// function filtrar_multiplos_5(){
+//     return n % 5 === 0;
+// }
+// var ret = numeros.filter(filtrar_pares)
+// console.log(ret);
+
+// ret=numeros.filter(filtrar_impares);
+// console.log(ret)
+
+// ret = numeros.filter(filtrar_multiplos_5);
+//console.log(ret);
+
+var alunos = [
+    {nome: "pedro", nota: 8.5},
+    {nome: "maria", nota: 10},
+    {nome: "fernanda" , nota: 9.2},
+    {nome: "ricardo", nota: 7.3}
+]
+function filtrar_notas_maiores_que_8(aluno){
+    return aluno.nota > 8;
+}
+
+var filtrados = alunos.filter(filtrar_notas_maiores_que_8);
+console.log(filtrados);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

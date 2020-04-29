195b425fbe5c0c1466279f249c2295eef7736007
Juliana Gonçalves <julianaxavier246@gmail.com>
2020-04-29 15:53:37 -0300

    reduce

--- Seção 5/index_3.js
+++ Seção 5/index_3.js
@@ -1,23 +1,23 @@
-// var numeros=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// function filtrar_pares(n){
-//     return n % 2 === 0;
-// }
-// //console.log(filtrar_pares(3));
-// console.log(filtrar_pares(8));
-// function filtrar_impares(n){
-//     return n % 2 !== 0;
-// }
-// function filtrar_multiplos_5(){
-//     return n % 5 === 0;
-// }
-// var ret = numeros.filter(filtrar_pares)
-// console.log(ret);
+var numeros=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function filtrar_pares(n){
+    return n % 2 === 0;
+}
+//console.log(filtrar_pares(3));
+console.log(filtrar_pares(8));
+function filtrar_impares(n){
+    return n % 2 !== 0;
+}
+function filtrar_multiplos_5(){
+    return n % 5 === 0;
+}
+var ret = numeros.filter(filtrar_pares)
+console.log(ret);
 
-// ret=numeros.filter(filtrar_impares);
-// console.log(ret)
+ret=numeros.filter(filtrar_impares);
+console.log(ret)
 
-// ret = numeros.filter(filtrar_multiplos_5);
-//console.log(ret);
+ret = numeros.filter(filtrar_multiplos_5);
+console.log(ret);
 
 var alunos = [
     {nome: "pedro", nota: 8.5},
---
+++ Seção 5/index_4.js
@@ -0,0 +1,30 @@
+var precos = [4.66, 3.78, 9.78, 1.34, 5.32];
+var soma = 0;
+
+for(var i = 0; i < precos.length; i++){
+    soma = soma + precos[i];
+}
+// //console.log(soma);
+soma = 0;
+ soma.forEach(function(valor){
+    soma += valor;  // soma = soma + valor
+});
+// // console.log(soma);
+
+function somar(anterior, atual){
+    return anterior + atual;
+}
+
+var ret = precos.reduce(somar);
+// console.log(ret);
+function adicionar_taxa(valor){
+    return valor + 5;
+}
+var ret = precos.map(adicionar_taxa).reduce(somar);
+// console.log(ret);
+function precp_maior_que_4(valor){
+    return valor > 4;
+}
+
+ret = precos.filter(precp_maior_que_4).map(adicionar_taxa).reduce(somar);
+console.log(ret);

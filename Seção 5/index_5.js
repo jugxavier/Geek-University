30c92b9d64d15b95f4d2126f9a13c566e2791218
Juliana Gonçalves <julianaxavier246@gmail.com>
2020-04-30 22:02:58 -0300

    destructuring

---
+++ Seção 5/index_5.js
@@ -0,0 +1,55 @@
+const numero=[1,3,5,7];
+
+const[num1,num2,num3,num4]=numero;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+// const [n1, ,n3,n4]=numero
+
+//  console.log(n1);
+//  console.log(n3);
+//  console.log(n4);
+
+const [nu1,nu2,nu3,nu4=34, nu5=99]=numero;
+// console.log(nu1);
+// console.log(nu2);
+// console.log(nu3);
+// console.log(nu4);
+// console.log(nu5);
+
+// destruturing com objeto
+const produto={
+    nome:"playtation 4",
+    preco:1678.44
+}
+// const{nome,preco}=produto;
+
+// console.log(nome);
+// console.log(preco);
+// console.log(`${nome}custa ${preco}`);
+
+const {nome}=produto;
+console.log(nome);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

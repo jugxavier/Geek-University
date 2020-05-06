e3af2e09b59f6002e8bc67818639b133b1440b3d
Juliana Gonçalves <julianaxavier246@gmail.com>
2020-05-05 22:16:09 -0300

    arrow function

---
+++ Seção 6/index_4.js
@@ -0,0 +1,29 @@
+// function somar(num1,num2){
+//     return num1 + num2;
+// }
+// // console.log(4,6);
+
+// let somar2 = function(num2,num2){
+//     return num1 + num2;
+// }
+// // console.log(somar2(4,6));
+ 
+// let somar3 = somar3;
+// console.log(somar3(4,6));
+
+// arrow function 
+let somar4 = (num1,num2) =>{
+    return num1 + num2;
+}
+console.log(somar4(4,6));
+
+let dobrar = valor => valor * 2;
+console.log(dobrar(3));
+
+function message(){
+    console.log("juliana")
+}
+message()
+
+const msg = () => console.log("juliana");
+msg();

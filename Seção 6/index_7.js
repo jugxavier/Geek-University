bf8e41b083f6476e37510c206c5d110492e877f7
Juliana Gonçalves <julianaxavier246@gmail.com>
2020-05-14 19:02:29 -0300

    update

--- Seção 6/index_7.js
+++ Seção 6/index_7.js
@@ -1,20 +1,33 @@
-function pessoa(n,s){
+function pessoa(n, s, raca = "humano"){
     let altura = 0.30;
     let peso = 0.5;
     let idade = 0;
-    let nome  = 0;
+    let nome  = n;
     let sexo= s;
 
-    this.raca="humano";
+    this.raca=raca;
  
-    let imprimirr_dados = function(){
-        console.log(`nome: ${nome}, idade: ${nome}, peso: ${peso}, altura: ${altura}, sexo: ${sexo}`);
+    let imprimir_dados = function(){
+        console.log(`nome: ${nome}, idade: ${idade}, peso: ${peso}, altura: ${altura}, sexo: ${sexo}, raça: ${raca}`);
     }
     this.fazer_aniversario = function(){
-        idade <= i;
-        imprimirr_dados();
+        idade += 1;
+        imprimir_dados();
     }
     this.getIdade = function(){
         return idade;
     }
 }
+const juliana = new pessoa("juliana","feminino");
+
+console.log(juliana);
+console.log(juliana.peso)
+console.log(juliana.raca);
+juliana.fazer_aniversario();
+juliana.fazer_aniversario();
+
+// const felicity = new pessoa("felicity","feminino");
+// felicity.fazer_aniversario();
+// console.log(felicity.getIdade());
+console.log(typeof(pessoa));
+console.log(typeof(felicity));
